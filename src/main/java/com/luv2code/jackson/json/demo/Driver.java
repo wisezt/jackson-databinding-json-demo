14069b547136076915e3e2ad0228eb0637284abb
Ting <wisezt@hotmail.com>
2019-05-01 20:13:47 -0500

    Add package and two class: Driver and Student

---
+++ src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -0,0 +1,27 @@
+package com.luv2code.jackson.json.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class Driver {
+
+    public static void main(String[] args){
+        try{
+            // create object mapper
+            ObjectMapper mapper = new ObjectMapper();
+
+            // reac JSON file and map/convert to Java/convert to Java POJO:
+
+            Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+
+            // print first name and last name
+            System.out.println("First name = " + theStudent.getFirstName());
+            System.out.println("Last name = " + theStudent.getLastName());
+        }
+        catch (Exception exc){
+            exc.printStackTrace();
+
+        }
+    }
+}
