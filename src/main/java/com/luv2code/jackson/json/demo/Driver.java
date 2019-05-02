582a4c12429fa12decf252aee2b6a42e0146cd7d
Ting <wisezt@hotmail.com>
2019-05-01 20:26:32 -0500

    print out Address and Languages

--- src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -18,7 +18,20 @@ public class Driver {
             // print first name and last name
             System.out.println("First name = " + theStudent.getFirstName());
             System.out.println("Last name = " + theStudent.getLastName());
+
+            // print out address: street and city
+            Address tempAddress = theStudent.getAddress();
+
+            System.out.println("Street = " + tempAddress.getStreet());
+            System.out.println("City = " + tempAddress.getCity());
+
+            // print out languages
+            for (String tempLang : theStudent.getLanguages()){
+                System.out.println(tempLang);
+            }
         }
+
+
         catch (Exception exc){
             exc.printStackTrace();
 
